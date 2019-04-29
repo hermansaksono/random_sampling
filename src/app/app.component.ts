a34e172c6c04d0e6695915b9b8a5f8fcc851a205
Herman Saksono <hsaksono@ccs.neu.edu>
2019-04-29 14:52:23 -0400

    Improve the clarity of the texts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Meta} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,23 @@ import { Title } from '@angular/platform-browser';
 })
 export class AppComponent {
   title = 'Sampel Acak (Random Sampling)';
-  public constructor(private titleService: Title ) {
+  desc = 'Mencoba cara kerja sampel acak (random sampling) yang dipakai untuk statistik (mis. survei, quickcount)';
+  imgUrl = 'https://hermansaksono.github.io/random_sampling/assets/facebook-img.png';
+  imgAlt = 'Apel dan jeruk';
+  public constructor(private titleService: Title, private meta: Meta ) {
     this.titleService.setTitle(this.title);
+    // Twitter metadata
+    meta.addTag({name: 'twitter:card', content: 'summary_large_image'});
+    meta.addTag({name: 'twitter:title', content: this.title});
+    meta.addTag({name: 'twitter:description', content: this.desc});
+    meta.addTag({name: 'twitter:image', content: this.imgUrl});
+    meta.addTag({name: 'twitter:image:alt', content: this.imgAlt});
+    // Facebook metadata
+    meta.addTag({name: 'og:type', content: 'article'});
+    meta.addTag({name: 'og:url', content: 'https://hermansaksono.github.io/random_sampling/'});
+    meta.addTag({name: 'og:title', content: this.title});
+    meta.addTag({name: 'og:description', content: this.desc});
+    meta.addTag({name: 'og:image', content: this.imgUrl});
+    meta.addTag({name: 'og:site_name', content: 'Random sampling'});
   }
 }
