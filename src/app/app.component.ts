42d0518de9f9ad6863a89f366914878bd21069f1
Herman Saksono <hsaksono@ccs.neu.edu>
2019-04-29 19:02:56 -0400

    Move meta tags into the index.html file.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,19 +14,5 @@ export class AppComponent {
   imgAlt = 'Apel dan jeruk';
   public constructor(private titleService: Title, private meta: Meta ) {
     this.titleService.setTitle(this.title);
-    // Twitter metadata
-    meta.addTag({name: 'twitter:card', content: 'summary_large_image'});
-    meta.addTag({name: 'twitter:title', content: this.title});
-    meta.addTag({name: 'twitter:description', content: this.desc});
-    meta.addTag({name: 'twitter:image', content: this.imgUrl});
-    meta.addTag({name: 'twitter:image:alt', content: this.imgAlt});
-    // Facebook metadata
-    meta.addTag({name: 'og:type', content: 'website'});
-    meta.addTag({name: 'og:url', content: 'https://hermansaksono.github.io/random_sampling/'});
-    meta.addTag({name: 'og:title', content: this.title});
-    meta.addTag({name: 'og:description', content: this.desc});
-    meta.addTag({name: 'og:image', content: this.imgUrl});
-    meta.addTag({name: 'og:site_name', content: 'Random sampling'});
-    meta.addTag({name: 'fb:app_id', content: 'https://hermansaksono.github.io/random_sampling/'});
   }
 }
